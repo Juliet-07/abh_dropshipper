22c38c01a8795cbf8f084b02426cd7c6772f85f7
Juliet-07 <ohankwerejulietkelechi@gmail.com>
2024-11-08 15:28:42 +0200

    image config

--- next.config.js
+++ next.config.js
@@ -63,6 +63,7 @@ module.exports = withPWA({
       "images.dashter.com",
       "adhproject.blob.core.windows.net",
       "abhfiles.blob.core.windows.net",
+      "abhfilestorage.blob.core.windows.net",
     ],
   },
   
